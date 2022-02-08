fe0d446f58862ac3268d64bb31b4d2a9dd741a9d
Ziyang Liu <lzy968@gmail.com>
2022-02-07 23:20:46 -0800

    modified the page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img height="200" width="200" alt="Vue logo" src="./assets/profile.jpg">
+  <HelloWorld msg="Ziyang Liu"/>
 </template>
 
 <script>
